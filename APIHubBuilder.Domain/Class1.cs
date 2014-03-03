f71bb3189befed2ebb471b5493ee7428bd37b020
IsaacGarcia <isaac@workker.com.br>
2014-03-03 12:54:25 -0300

    Estrutura inicial do projeto

---
+++ APIHubBuilder.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIHubBuilder.Domain
+{
+    public class Class1
+    {
+    }
+}
